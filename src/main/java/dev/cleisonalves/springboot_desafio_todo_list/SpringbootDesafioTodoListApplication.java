078c8f0e3377081bd9f1430110acc33199659455
Cleison Alves <142464083+TchecoAlves@users.noreply.github.com>
2025-08-28 23:39:08 -0300

    feat: Inicio Projeto

---
+++ src/main/java/dev/cleisonalves/springboot_desafio_todo_list/SpringbootDesafioTodoListApplication.java
@@ -0,0 +1,13 @@
+package dev.cleisonalves.springboot_desafio_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDesafioTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDesafioTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cleisonalves/springboot_desafio_todo_list/SpringbootDesafioTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cleisonalves.springboot_desafio_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDesafioTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
